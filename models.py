e85ca816101befb97da5a05a99fdcb6e8224c0f3
Bjørnar Østtveit <bjosttveit@gmail.com>
2021-12-03 13:36:24 +0100

    updated models and dataset

--- models.py
+++ models.py
@@ -4,16 +4,16 @@ import wandb
 api = wandb.Api()
 
 runs = [
-    {"artifact": "run_1ee986s2_model:v0", "model": "n"},
-    {"artifact": "run_3u9pn5lp_model:v0", "model": "n6"},
-    {"artifact": "run_q6s5egmt_model:v0", "model": "s"},
-    {"artifact": "run_w8w8z8bs_model:v0", "model": "s6"},
-    {"artifact": "run_2lc4ubop_model:v0", "model": "m"},
-    {"artifact": "run_37kan8fz_model:v0", "model": "m6"},
-    {"artifact": "run_129dpchz_model:v0", "model": "l"},
-    {"artifact": "run_1x8y5m2a_model:v0", "model": "l6"},
-    {"artifact": "run_1owjyymy_model:v0", "model": "x"},
-    {"artifact": "run_2u9mohkc_model:v0", "model": "x6"},
+    {"artifact": "run_127f7oit_model:v0", "model": "n"},
+    {"artifact": "run_uz30yl46_model:v0", "model": "n6"},
+    {"artifact": "run_3b9by2ch_model:v0", "model": "s"},
+    {"artifact": "run_2d0l7aln_model:v0", "model": "s6"},
+    {"artifact": "run_2k6ao01f_model:v0", "model": "m"},
+    {"artifact": "run_31kwfi31_model:v0", "model": "m6"},
+    {"artifact": "run_1te89l0r_model:v0", "model": "l"},
+    {"artifact": "run_2f5f27sr_model:v0", "model": "l6"},
+    {"artifact": "run_co6roviu_model:v0", "model": "x"},
+    {"artifact": "run_j7xy9jp0_model:v0", "model": "x6"},
 ]
 
 if not os.path.exists('models'):
